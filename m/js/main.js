e2a98a28bde447164cd52bb2dfef1d5e8179c6e2
TremendaCarucha <tremendacarucha@gmail.com>
2023-12-04 19:24:58 -0300

    Add sharing and restoring functionality

--- m/js/main.js
+++ m/js/main.js
@@ -2,11 +2,12 @@
   const lines = document.querySelectorAll('.line')
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i]
-    const fitted = fitty('#' + line.id + ' span', {
+    const span = line.querySelector('span')
+    const fitted = fitty('#' + span.id, {
       multiLine: false, maxSize: 180
     })[0]
     line.addEventListener('click', (e) => {
-      fitted.element.focus()
+      span.focus()
     })
     line.addEventListener('keydown', (e) => {
       if (e.keyCode === 13) {
@@ -17,19 +18,48 @@
     fitted.fit()
   }
 
-  const bgColor = document.getElementById('bg-color')
-  const textColor = document.getElementById('text-color')
-
+  const bg = document.getElementById('bg')
   const updateBg = () => {
-    document.getElementById('bg').style.background = bgColor.value
+    document.getElementById('background').style.background = bg.value
   }
-  const updateText = () => {
-    document.getElementById('lines').style.color = textColor.value
-  }
-
-  bgColor.addEventListener('change', updateBg)
-  textColor.addEventListener('change', updateText)
+  bg.addEventListener('change', updateBg)
   updateBg()
+  
+  const fg = document.getElementById('fg')
+  const updateText = () => {
+    document.getElementById('lines').style.color = fg.value
+  }
+  fg.addEventListener('change', updateText)
   updateText()
 
+  const IDS = ['l1', 'l2', 'l3', 'bg', 'fg']
+
+  document.getElementById('share').onclick = () => {
+    const vals = IDS
+      .map((id) => {
+        const elem = document.getElementById(id)
+        const val = elem.value || elem.innerText
+        return val && id + '=' + encodeURIComponent(val)
+      })
+      .filter(v => !!v)
+
+    location.search = '?' + vals.join('&')
+  }
+
+  location.search.slice(1).split('&').forEach((s) => {
+    if (!s) {
+      return
+    }
+    const parts = s.split('=')
+    const id = parts[0]
+    const val = decodeURIComponent(parts[1])
+    const elem = document.getElementById(id)
+    if (id) {
+      if ('value' in elem) {
+        elem.value = val
+      } else {
+        elem.innerText = val
+      }
+    }
+  })
 })()
